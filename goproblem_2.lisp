b34439060eb907a7819ac1c84ac3ef2d0f50da64
arbolis <anjeeisaac@gmail.com>
2015-04-22 18:04:18 -0300

    yet another update

--- goproblem_2.lisp
+++ goproblem_2.lisp
@@ -7,6 +7,9 @@
 ;; I use defparameter instead of defvar so that I can reset the matrix by using this same line
 (defparameter *board* (make-array (list (+ *n* 2) (+ *n* 2)) :initial-element 0))
 
+;; Set counter to 0
+(setf counter 0)
+
 ;; Generate random coordinates
 (defun random-coordinate ()
   (+ 1 (random *n*)))   
@@ -16,26 +19,27 @@
 (defparameter *y-coordinate* (random-coordinate))
 
 ;; Define a boolean function that checks whether there are adjascent stones
-(defun mth (b i j)
-  (nth (nth b i) j))
 
 (defun check-if-adjascent ()
-       (not (or
-	    (= 1 (mth *board* (decf *x-coordinate*) *y-coordinate*))
-	    (= 1 (mth *board* *x-coordinate* (decf *y-coordinate*)))
-	    (= 1 (mth *board* (incf *x-coordinate*) *y-coordinate*))
-	    (= 1 (mth *board* *x-coordinate* (incf *y-coordinate*))))))
+  (or
+   (= 1 (aref *board* (decf *x-coordinate*) *y-coordinate*))
+   (= 1 (aref *board* *x-coordinate* (decf *y-coordinate*)))
+   (= 1 (aref *board* (incf *x-coordinate*) *y-coordinate*))
+   (= 1 (aref *board* *x-coordinate* (incf *y-coordinate*)))))
 ;; Define a boolean function that checks whether there's already a stone placed there
 (defun already-a-stone ()
-  (not (= 1 (mth *board* *x-coordinate* *y-coordinate*))))
+  (= 1 (aref *board* *x-coordinate* *y-coordinate*)))
 
 ;; Now the serious and hardest part
-;;if check-if-adjascent returns false and there is no stone at x_coordinate y_coordinate, then
+;;if check-if-adjascent returns false and there is no stone at x-coordinate y-coordinate, then
 ;;add a new stone (see below) and increase the counter by 1.
-;;else if there is a stone at x_coordinate y_coordinate, pick a new x_coordinate y_coordinate
+;;else if there is a stone at x-coordinate y-coordinate, pick a new x_coordinate y_coordinate
 ;;else increase the counter and return "'(It took) counter '(stones)".
 
-(if already-a-stone )
+(if (and (not (check-if-adjascent)) (not (already-a-stone)))
+    (setf (aref *board* *x_coordinate* *y_coordinate*) 1)  ;body
+     (incf counter)							   ;else part
+    )   
 
 
 
